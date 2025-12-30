9cbef4accc9410f04c98382f26b63905ce04a4b2
Booth, Robert <rbooth30@gatech.edu>
2025-12-30 14:18:13 -0500

    Initial Spring Boot project structure commit.

---
+++ src/main/java/com/rb/osrspal/OsrspalApplication.java
@@ -0,0 +1,13 @@
+package com.rb.osrspal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsrspalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsrspalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rb/osrspal/OsrspalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rb.osrspal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsrspalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
